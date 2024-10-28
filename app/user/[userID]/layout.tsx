a18039cac1bc0029916e7b376fd0a71727cc5523
Nafim Ershad <inan.nafim1089@outlook.com>
2024-10-28 22:35:57 +0600

    Added equal spacing and made the settings tab same size

--- app/user/[userID]/layout.tsx
+++ app/user/[userID]/layout.tsx
@@ -40,37 +40,37 @@ function Layout({ children }: HTMLAttributes<HTMLDivElement> ): ReactNode {
     <div className='w-[95%] h-[98%] flex items-center justify-start shadow-lg rounded-3xl'>
       {/* Left Section */}
         <div className='md:w-full md:max-w-40 w-16 md:h-[95%] h-[50%] border-r-[1px] border-solid border-slate-300 flex flex-col items-center justify-center'>
-          <div className='px-2 py-4 flex flex-col items-start justify-between gap-4'>
+          <div className='px-2 py-4 flex flex-col items-center justify-between gap-4'>
             <Link 
-              className={`p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'overview' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
+              className={`md:w-full p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'overview' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
               href={`/user/${userID}/`}
             >
               <Eye/>
               <p className='md:inline-block hidden'>Overview</p>
             </Link>
             <Link 
-              className={`p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'account' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
+              className={`md:w-full p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'account' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
               href={`/user/${userID}/account`}
             >
               <CircleUser/>
               <p className='md:inline-block hidden'>Account</p>
             </Link>
             <Link 
-              className={`p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'orders' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
+              className={`md:w-full p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'orders' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
               href={`/user/${userID}/orders`}
             >
               <ScrollText/>
               <p className='md:inline-block hidden'>Orders</p>
             </Link>
             <Link 
-              className={`p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'payments' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
+              className={`md:w-full p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'payments' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
               href={`/user/${userID}/payments`}
             >
               <CreditCard/>
               <p className='md:inline-block hidden'>Payments</p>
             </Link>
             <Link 
-              className={`p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'addresses' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
+              className={`md:w-full p-2 rounded-xl flex items-center justify-start gap-4 border-solid border-2 hover:border-black  ${pageSettings.currentMenu === 'addresses' ? "bg-black text-white border-black" : "bg-white text-black border-white"}`}
               href={`/user/${userID}/addresses`}
             >
               <BookUser/>
