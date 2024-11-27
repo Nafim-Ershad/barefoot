81e3d622c0448f0e5c33b96e4f2ce85ec425c3d1
Nafim Ershad <inan.nafim1089@outlook.com>
2024-11-28 04:08:47 +0600

    Had to change the props types

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,12 @@
 import { HTMLAttributes, ReactNode } from "react";
 
-function Layout({ children }: HTMLAttributes<HTMLDivElement>): ReactNode{
+import { tUser } from "@/lib/type";
+
+interface iLayout extends HTMLAttributes<HTMLDivElement>{
+    user: tUser | null;
+}
+
+function Layout({ children }: iLayout): ReactNode{
     return(
         <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center text-black'>
             { children }
